d2f76b330c059d738973dfc92f884fd9f2de4260
GooseXP <zka719@gmail.com>
2021-08-06 21:15:43 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -11,7 +11,7 @@ DISCOUNT = 0.99
 REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
 MIN_REPLAY_MEMORY_SIZE = 100000  # Minimum number of steps in a memory to start training
 MINIBATCH_SIZE = 1000  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 5  # Update Target model after x steps
+UPDATE_TARGET_EVERY = 10  # Update Target model after x steps
 MEMORY_FRACTION = 0.20
 
 # Exploration settings
